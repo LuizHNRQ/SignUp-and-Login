424771788c4722c9bf0b4c11d2eb5cf020848988
Luiz Henrique Barros Teixeira <luizhbtluiz@gmail.com>
2020-04-09 12:31:34 -0300

    Added Javascript funcionalities to verify fields

--- scripts.js
+++ scripts.js
@@ -9,25 +9,65 @@ const password2 = document.querySelector('#password2');
 function isEmpty(input) {
   input.forEach(entry => {
     if (entry.value === '') {
-      showError(entry, 'Cannot be Empty');
+      showError(entry, 'Campo não pode ser vazio');
     } else {
       showSuccess(entry);
     }
   });
 }
 
-//show success or error
+// check if its in the correrct length
 
-function showSuccess(input) {
-  const parentClass = input.parentElement.classList;
-  if (parentClass.contains('error')) {
-    parentClass.remove('error');
-    parentClass.add('success');
+function checkLength(input, min, max) {
+  const entry = input.value.length;
+  if (entry == 0) {
+
+  } else if (entry < min) {
+    showError(input, `Tem que ter ao menos ${min} caracteres`);
+  } else if (entry > max) {
+    showError(input, `Tem que ter no maximo ${max} caracteres`);
   } else {
-    parentClass.add('success');
+    showSuccess(input);
   }
 }
 
+//check email is valid
+
+function checkEmail(input) {
+  var regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (regex.test(input.value.toLowerCase())) {
+    showSuccess(input);
+  } else {
+    showError(input, 'Email inválido');
+  }
+
+}
+
+//check password is equal
+
+function checkPassword(input1, input2) {
+  if (input2.value === input1.value) {
+    showSuccess([input1, input2]);
+  } else {
+    showError(input2, 'As senhas não são iguais')
+  }
+}
+//show success 
+
+function showSuccess(input) {
+  if (input.parentElement !== undefined) {
+    const parentClass = input.parentElement.classList;
+    if (parentClass.contains('error')) {
+      parentClass.remove('error');
+      parentClass.add('success');
+    } else {
+      parentClass.add('success');
+    }
+  }
+}
+
+//show Error 
 function showError(input, message) {
   input.parentElement.classList.add('error');
   input.parentElement.querySelector('small').innerText = `${message}`;
@@ -37,4 +77,8 @@ function showError(input, message) {
 form.addEventListener('submit', e => {
   e.preventDefault();
   isEmpty([user, email, password, password2]);
+  checkLength(user, 3, 12);
+  checkLength(password, 6, 12);
+  checkEmail(email);
+  checkPassword(password, password2);
 });
